7688d088c83aedc095e8fcd3f5dfe5ebbf5c7fd0
Rob Lavender <robert.lavender@digital.hmrc.gov.uk>
2015-12-02 14:06:04 +0000

    Add user pass details.

--- app/discovery/app.js
+++ app/discovery/app.js
@@ -4,4 +4,5 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
+
 module.exports = app;
--- global/admin/app.js
+++ global/admin/app.js
@@ -3,4 +3,6 @@ var app = require('../../lib/subapp.js')(__dirname);
 require('./lib/new-iteration.js')(app);
 require('./lib/edit-iteration.js')(app);
 
+app.use(express.basicAuth('team9', 't34m9-hmrc'));
+
 module.exports = app;
