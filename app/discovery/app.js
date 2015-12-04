aae6dbf99be6f52ecff64cd64f9fb0467f736b00
Rob Lavender <robert.lavender@digital.hmrc.gov.uk>
2015-12-04 14:58:19 +0000

    Add option to start page to input repayment amount.

--- app/discovery/app.js
+++ app/discovery/app.js
@@ -4,5 +4,15 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
+// Post request
+// Get a post value from index
+
+app.post('/check-details', function (req, res) {
+  var repayAmount = req.body.repayment;
+  //console.log( "The repayment amount is " + repayment)
+  res.render('check-details', {repayment: repayAmount} );
+  //res.redirect('check-details', {title: 'Home page'});
+});
+
 
 module.exports = app;
