d05beca01d516d0f0f45806a81d4222f095c4b1b
Rob Lavender <robert.lavender@digital.hmrc.gov.uk>
2015-12-07 13:49:22 +0000

    Add an extra page to check submitted bank details.

--- app/discovery/app.js
+++ app/discovery/app.js
@@ -7,11 +7,28 @@ app.get('/', function (req, res) {
 // Post request
 // Get a post value from index
 
-app.post('/check-details', function (req, res) {
+app.post('/bank-account', function (req, res) {
   var repayAmount = req.body.repayment;
   //console.log( "The repayment amount is " + repayment)
-  res.render('check-details', {repayment: repayAmount} );
-  //res.redirect('check-details', {title: 'Home page'});
+  res.render('bank-account', {repayment: repayAmount} );
+});
+
+
+app.post('/check-details', function (req, res) {
+
+  //console.log(req.body)
+  var accountNumber = req.body.accountnumber;
+  var sortcode1 = req.body.sortcode1;
+  var sortcode2 = req.body.sortcode2;
+  var sortcode3 = req.body.sortcode3;
+  //console.log( "The repayment amount is " + repayment)
+  res.render('check-details', {
+    accountNumber: accountNumber,
+    sortcode1: sortcode1,
+    sortcode2: sortcode2,
+    sortcode3: sortcode3}
+  );
+  //res.render('check-details', {sortcode1: sortcode1});
 });
 
 
